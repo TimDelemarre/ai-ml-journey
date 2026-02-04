3475f7b231a5062e08aba1ce1ab3220e7682d9b8
TimDelemarre <tim.delemarre@gmail.com>
2026-02-03 23:11:45 -0800

    week1: variables and basic types

---
+++ week1_basics.py
@@ -0,0 +1,11 @@
+name = "Tim"
+field = "Immunology"
+years_experience =6
+
+print(name)
+print(field)
+print(years_experience)
+
+print (type(name))
+print (type(field))
+print (type(years_experience))
